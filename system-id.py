93f2070255a48f5bac6e48b3cebb7f44cfe9f583
Piotr Blaszyk <piopio555888@gmail.com>
2025-07-15 22:49:50 +0100

    experimentation

--- system-id.py
+++ system-id.py
@@ -14,10 +14,13 @@ class SystemId:
         self.f = DobotApiFeedBack(ip, feedback_port)
         self.feedback = dict()
 
+        self.home_pos_np = np.array([-350, -25, 82, 180, 0, 0])
         self.set_trajectories()
 
         print(self.d.RequestControl())
         print(self.d.EnableRobot())
+        print(self.d.SetCollisionLevel(0))
+        print(self.d.SetBackDistance(0))
 
         threading.Thread(target=self.get_feed_small, daemon=True).start()
     
@@ -50,21 +53,21 @@ class SystemId:
     
     
     def set_trajectories(self):
-        self.home_pos_np = np.array([-310, -25, 63, 180, 0, 0])
         x, y, z, xr, yr, zr = self.home_pos_np
-        press_depth = 6
-        slide_length = 25
+        press_depth = 4
+        slide_length = 50
         twist_x = 20
         twist_z = 20
         self.trajectories_np = np.array([
             [
+                [x, y, z, xr, yr, zr],
                 [x, y, z, xr, yr, zr],
                 [x, y, z-press_depth, xr, yr, zr],
             ],
             [
                 [x, y, z, xr, yr, zr],
                 [x, y, z-press_depth, xr, yr, zr],
-                [x-slide_length, y, z-press_depth, xr, yr, zr],
+                [x-slide_length, y, z-press_depth+2, xr, yr, zr],
             ],
             [
                 [x, y, z, xr, yr, zr],
@@ -81,11 +84,16 @@ class SystemId:
     def execute_trajectory(self, trajectory_ix):
         for i in range(self.trajectories_np[trajectory_ix].shape[0]):
             self.run_point(
-                self.dMovL(*self.trajectories_np[trajectory_ix][i].tolist(), coordinateMode=0, speed=2, v=2)
+                self.d.MovL(*self.trajectories_np[trajectory_ix][i].tolist(), coordinateMode=0, speed=3)
             )
+            print(f'target {i} reached!')
+        sleep(0.5)
+        self.run_home_pos()
 
     def run_home_pos(self):
-        self.run_point(self.d.MovJ(*self.home_pos_np.tolist(), coordinateMode=0, speed=2, v=2))
+        self.run_point(self.d.MovJ(*self.home_pos_np.tolist(), coordinateMode=0, v=10))
+        print('home position reached!')
+        print()
 
 if __name__ == '__main__':
     s = SystemId()
