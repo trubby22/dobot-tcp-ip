9df1d0b0a159e78c34752bcbf3f04f9f6656bce1
Piotr Blaszyk <piopio555888@gmail.com>
2025-07-16 21:29:19 +0100

    take exp videos

--- system-id.py
+++ system-id.py
@@ -24,12 +24,6 @@ class SystemId:
         print(self.d.Tool(2))
 
         threading.Thread(target=self.get_feed_small, daemon=True).start()
-    
-    def parse_result_id(self, response):
-        if "Not Tcp" in response:
-            print("Control Mode Is Not Tcp")
-            return [1]
-        return [int(num) for num in re.findall(r'-?\d+', response)] or [2]
 
     def parse_pose(self, pose_str):
         pose_part = pose_str.split('{')[1].split('}')[0]
@@ -66,30 +60,37 @@ class SystemId:
     def set_trajectories(self):
         self.home_pos_np = np.array(self.parse_pose(self.d.GetPose()))
         x, y, z, xr, yr, zr = self.home_pos_np
-        press_depth = 4
+        press_depth_0 = 10
+        press_depth_1 = 4
+        press_depth_2 = 6
+        press_depth_3 = 8
         slide_length = 50
-        twist_x = 20
-        twist_z = 20
+        twist_x = 30
+        twist_z = 45
         self.trajectories_np = np.array([
             [
                 [x, y, z, xr, yr, zr],
                 [x, y, z, xr, yr, zr],
-                [x, y, z-press_depth, xr, yr, zr],
+                [x, y, z-press_depth_0, xr, yr, zr],
+                [x, y, z, xr, yr, zr],
             ],
             [
                 [x, y, z, xr, yr, zr],
-                [x, y, z-press_depth, xr, yr, zr],
-                [x-slide_length, y, z-press_depth, xr, yr, zr],
+                [x, y, z, xr, yr, zr],
+                [x, y, z-press_depth_1, xr, yr, zr],
+                [x-slide_length, y, z-press_depth_1, xr, yr, zr],
             ],
             [
                 [x, y, z, xr, yr, zr],
-                [x, y, z-press_depth, xr, yr, zr],
-                [x, y, z-press_depth, xr+twist_x, yr, zr],
+                [x, y, z, xr, yr, zr],
+                [x, y, z-press_depth_2, xr, yr, zr],
+                [x, y, z-press_depth_2, xr+twist_x, yr, zr],
             ],
             [
                 [x, y, z, xr, yr, zr],
-                [x, y, z-press_depth, xr, yr, zr],
-                [x, y, z-press_depth, xr, yr, zr+twist_z],
+                [x, y, z-press_depth_3, xr, yr, zr],
+                [x, y, z-press_depth_3, xr, yr, zr+twist_z],
+                [x, y, z, xr, yr, zr+twist_z],
             ],
         ])
     
@@ -107,13 +108,6 @@ class SystemId:
             vel[:3] = np.clip(vel[:3], -v_pos, v_pos)
             vel[3:] = np.clip(vel[3:], -v_ori, v_ori)
             next_pose = pose + vel * 0.030
-            if False and t % 1 == 0:
-                print(f'pose: {pose}')
-                print(f'target_pose: {target_pose}')
-                print(f'error: {error}')
-                print(f'vel: {vel}')
-                print(f'next_pose: {next_pose}')
-                print()
             self.d.ServoP(*next_pose.tolist())
             sleep(0.030)
             t += 1
@@ -130,15 +124,14 @@ class SystemId:
             )
             print(f'target {i} reached!')
         sleep(0.5)
-        self.run_home_pos(10)
 
-    def run_home_pos(self, v):
-        self.run_point(self.d.MovJ(*self.home_pos_np.tolist(), coordinateMode=0, v=v))
+    def run_home_pos(self):
+        self.run_point(self.d.MovJ(*self.home_pos_np.tolist(), coordinateMode=0, v=20))
         print('home position reached!')
         print()
     
-    def run_upright_pos(self, v):
-        self.run_point(self.d.MovJ([0] * 6, coordinateMode=1, v=v))
+    def run_upright_pos(self):
+        self.run_point(self.d.MovJ(*np.zeros(shape=(6,)).tolist(), coordinateMode=1, v=20))
         print('home position reached!')
         print()
 
